7bce8f0acafbaa3799e30740cb3300a9d3c4f955
John Wu <john.wu@mail.mcgill.ca>
2017-03-17 18:59:55 -0400

    Refactoring

--- purple-apple/api/facebook.js
+++ purple-apple/api/facebook.js
@@ -2,6 +2,7 @@
  * Created by JohnWu on 2017-03-17.
  */
 var request = require('request');
+var Promise = require('promise');
 
 module.exports = {
     search: search
@@ -12,21 +13,28 @@ function search() {
     var query = "macdonald";
     var clientId = '641741966019779';
     var clientSecret = '14d89dc6658614ffd23cb6ca314581df';
-    var url = "https://graph.facebook.com/search?fields=id,name,location,overall_rating&q=" + query + "&type=place&center=45.525476,%20-73.574596&distance=10000&access_token=" + clientId + "|" + clientSecret
+    var url = "https://graph.facebook.com/search?fields=id,name,location,overall_rating&q=" + query + "&type=place&center=45.525476,%20-73.574596&distance=10000&access_token=" + clientId + "|" + clientSecret;
 
-    var token = '641741966019779|tRYxL3sSUqUDa94vj6k8NwyDFJA';
 
-    request.get(url,function(error,response,body){
-        console.log("Finished get");
-        if(error){
-            console.log("Error reached");
-            return error;
-        }else{
-            console.log("Response reached");
-            console.log(response);
-            return response;
-        }
+    // request.get(url,function(error,response,body){
+    //     console.log("Finished get");
+    //     if(error){
+    //         console.log("Error reached");
+    //         return error;
+    //     }else{
+    //         console.log("Response reached");
+    //         console.log(response);
+    //         return response;
+    //     }
+    // });
+
+    return new Promise(function (fulfill, reject){
+        request.get(url,function(error,response,body){
+            if (error) reject(error);
+            else fulfill(response);
+        });
     });
+
 }
 
 
