32a912778452011348099722d39f64ce2672a7e1
John Wu <john.wu@mail.mcgill.ca>
2017-03-17 19:12:24 -0400

    Deleting commented code

--- purple-apple/api/facebook.js
+++ purple-apple/api/facebook.js
@@ -15,19 +15,6 @@ function search() {
     var clientSecret = '14d89dc6658614ffd23cb6ca314581df';
     var url = "https://graph.facebook.com/search?fields=id,name,location,overall_rating&q=" + query + "&type=place&center=45.525476,%20-73.574596&distance=10000&access_token=" + clientId + "|" + clientSecret;
 
-
-    // request.get(url,function(error,response,body){
-    //     console.log("Finished get");
-    //     if(error){
-    //         console.log("Error reached");
-    //         return error;
-    //     }else{
-    //         console.log("Response reached");
-    //         console.log(response);
-    //         return response;
-    //     }
-    // });
-
     return new Promise(function (fulfill, reject){
         request.get(url,function(error,response,body){
             if (error) reject(error);
