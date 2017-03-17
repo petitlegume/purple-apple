1e4642c9c844c01f900f1380e0648153cd235d1c
francisct <franciscotetremblay@gmail.com>
2017-03-17 18:17:42 -0400

    added basic category filtering functionality prototype

---
+++ purple-apple/dataTemplates/aw.js
@@ -0,0 +1,140 @@
+
+module.exports = {
+    data: {
+        "loc_brandname": "B&W",
+        "loc_name": "B&W",
+        "loc_addr1": "1355 Lougheed Highway",
+        "loc_addr2": "",
+        "loc_city": "Coquitlam",
+        "loc_state": "BC",
+        "loc_zip": "V3K 6P9",
+        "loc_country": "CA",
+        "loc_mainphone": "6045171191",
+        "loc_secphone": null,
+        "loc_fax": null,
+        "loc_email": "",
+        "loc_trackedwebsite": "http://aw.ca/",
+        "loc_publishedwebsite": "http://aw.ca/",
+        "loc_cat": "Hamburger Restaurant, Breakfast Restaurant, Fast Food Restaurant, American Restaurant, Restaurant",
+        "loc_hours": "Sunday 00:00:24:00, Monday 00:00:24:00, Tuesday 00:00:24:00, Wednesday 00:00:24:00, Thursday 00:00:24:00, Friday 00:00:24:00, Saturday 00:00:24:00",
+        "loc_hoursadd": "",
+        "loc_payments": null,
+        "loc_descsnippet": "Canada's original burger restaurant. Serving great-tasting burgers, handmade onion rings, and frosted mugs of famous A&W Root Beer since 1956.",
+        "loc_descshort": null,
+        "loc_desclong": "Canada's original burger restaurant. Serving great-tasting burgers, handmade onion rings, and frosted mugs of famous A&W Root Beer since 1956. At A&W, we're on a journey to source simple, great-tasting ingredients, farmed with care. We're proud to have high standards when it comes to our food and you can taste it in everything we serve. It's all part of our commitment to serve you food you can feel good about. For us, great burgers come first. So it's only natural that we're the first and only national burger restaurant in Canada to serve beef raised without the use of hormones or steroids. We're also the first and only national burger restaurant in Canada to serve chicken raised without the use of antibiotics. Our eggs are farmed right here in Canada, from hens fed a vegetarian diet without animal by-products. We're also proud to serve organic, Fairtrade coffee blended for us by Master Roaster Van Houtte.",
+        "loc_keywords": "burger, breakfast, chicken , restaurant, location, menu, coupons, root beer, Teen Burger, beef",
+        "loc_geo": "Coquitlam",
+        "loc_tags": "BC",
+        "loc_dirpackage": "AW Custom 15",
+        "loc_yearfounded": 2000,
+        "loc_info": "Drive Thru",
+        "loc_info2": "Bone-In Chubby Chicken",
+        "loc_logo": "",
+        "loc_banner": "https://swiq3locationreports.s3.amazonaws.com/ff7f0c850bd9b94ef7c31e371680a9cc92f7c4c065a6e638c35265efc90eef3c.png",
+        "loc_images": [],
+        "loc_sabunit": "",
+        "loc_sabdistance": 0,
+        "loc_sabregions": null,
+        "loc_providedlatitude": null,
+        "loc_providedlongitude": null,
+        "loc_status": 4,
+        "loc_distto": "",
+        "loc_distcc": "",
+        "loc_distnotes": null,
+        "loc_swiqlogin": null,
+        "loc_swiqpass": null,
+        "loc_latlong": "-122.856296,49.233707",
+        "loc_canceldate": null,
+        "loc_suppresslistings": null,
+        "loc_closeddoorsdate": null,
+        "loc_socialmediaurls": null,
+        "loc_alternativewebsites": null,
+        "loc_services": null,
+        "loc_createdAt": "2015-01-01T00:00:00.000Z",
+        "loc_updatedAt": "2017-03-11T05:55:20.000Z",
+        "loc_professional": null,
+        "loc_callTrackingNumber": null,
+        "loc_businessType": null,
+        "loc_latlongJson": null,
+        "categories": [
+            {
+                "id": "gcid:restaurant",
+                "name": "Restaurant"
+            },
+            {
+                "id": "gcid:american_restaurant",
+                "name": "American Restaurant"
+            },
+            {
+                "id": "gcid:breakfast_restaurant",
+                "name": "Breakfast Restaurant"
+            },
+            {
+                "id": "gcid:hamburger_restaurant",
+                "name": "Hamburger Restaurant"
+            },
+            {
+                "id": "gcid:fast_food_restaurant",
+                "name": "Fast Food Restaurant"
+            }
+        ],
+        "snippetDescription": {
+            "en": {
+                "lang": "en",
+                "countryCode": "US",
+                "content": "Canada's original burger restaurant. Serving great-tasting burgers, handmade onion rings, and frosted mugs of famous A&W Root Beer since 1956."
+            }
+        },
+        "longDescription": {
+            "en": {
+                "lang": "en",
+                "countryCode": "US",
+                "content": "Canada's original burger restaurant. Serving great-tasting burgers, handmade onion rings, and frosted mugs of famous A&W Root Beer since 1956. At A&W, we're on a journey to source simple, great-tasting ingredients, farmed with care. We're proud to have high standards when it comes to our food and you can taste it in everything we serve. It's all part of our commitment to serve you food you can feel good about. For us, great burgers come first. So it's only natural that we're the first and only national burger restaurant in Canada to serve beef raised without the use of hormones or steroids. We're also the first and only national burger restaurant in Canada to serve chicken raised without the use of antibiotics. Our eggs are farmed right here in Canada, from hens fed a vegetarian diet without animal by-products. We're also proud to serve organic, Fairtrade coffee blended for us by Master Roaster Van Houtte."
+            }
+        },
+        "parsedHours": {
+            "Sun": [
+                [
+                    "00:00",
+                    "00:00"
+                ]
+            ],
+            "Mon": [
+                [
+                    "00:00",
+                    "00:00"
+                ]
+            ],
+            "Tue": [
+                [
+                    "00:00",
+                    "00:00"
+                ]
+            ],
+            "Wed": [
+                [
+                    "00:00",
+                    "00:00"
+                ]
+            ],
+            "Thu": [
+                [
+                    "00:00",
+                    "00:00"
+                ]
+            ],
+            "Fri": [
+                [
+                    "00:00",
+                    "00:00"
+                ]
+            ],
+            "Sat": [
+                [
+                    "00:00",
+                    "00:00"
+                ]
+            ]
+        }
+    }
+}
