12919483e95c1d37cdd49b6b12be80b195f4a58b
John Wu <john.wu@mail.mcgill.ca>
2017-03-17 15:43:46 -0400

    sdfsf

--- purple-apple/api/facebook.js
+++ purple-apple/api/facebook.js
@@ -8,9 +8,10 @@ var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    var token = '14d89dc6658614ffd23cb6ca314581df';
+    var clientId = '641741966019779';
+    var clientSecret = '14d89dc6658614ffd23cb6ca314581df';
 
-    request.get("https://graph.facebook.com/search?q=ritual&type=place&center=37.76,-122.427&distance=1000&access_token=" + token ,function(error,response,body){
+    request.get("https://graph.facebook.com/oauth/access_token%20?client_id=" + clientId + "&client_secret=" + clientSecret + "&grant_type=client_credentials" + token ,function(error,response,body){
         if(error){
             res.send(error)
         }else{
