4e722e0b999a55cd63fbe29a8e62f1e89a491d2f
John Wu <john.wu@mail.mcgill.ca>
2017-03-17 17:20:30 -0400

    Refactoring

--- purple-apple/app.js
+++ purple-apple/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var facebook = require('./api/facebook');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/api/facebook', facebook);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
