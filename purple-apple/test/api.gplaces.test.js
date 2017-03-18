06af37bf245b6ce8cf3f672d4910d8055e0d134d
Jason Rudland <knowlecules@gmail.com>
2017-03-18 13:08:38 -0400

    Added testing and refactored places to add detail

---
+++ purple-apple/test/api.gplaces.test.js
@@ -0,0 +1,30 @@
+var gPlaces = require("../api/gPlaces.js");
+var expect = require('chai').expect;
+
+function ensureApiSearch(params, expectedResultsLength) {
+  return function (done) {
+  
+    gPlaces.search(params).then(actualResults => {
+      if(!expectedResultsLength) {
+        should(actualResults).be.null;
+      } else {
+        expect(expectedResultsLength).to.eql(actualResults.length);
+      }
+      done();
+    });
+  };
+};
+
+describe('gplaces api', function () {
+
+  describe('nearby', function () {
+    it('succeeds when valid arguments', ensureApiSearch({
+        loc: {
+            lat: 45.525,
+            lon: -73.57
+        },
+        radius: 1000,
+        category: "burger"
+        },  14));
+  });
+});
